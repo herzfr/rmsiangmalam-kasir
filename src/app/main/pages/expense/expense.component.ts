8f31558de53b087a7cdf983b2fa2003be9380c49
herzfr <47446320+herzfr@users.noreply.github.com>
2022-12-15 23:17:00 +0700

    Update

---
+++ src/app/main/pages/expense/expense.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'expense-app',
+    templateUrl: 'expense.component.html'
+})
+
+export class ExpenseComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
